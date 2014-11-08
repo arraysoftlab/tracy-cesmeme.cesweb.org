60bd3f2dc666883a72779f66fc42fc29b560e307
Me <xenon.mbstu@gmail.com>
2014-11-08 14:39:05 +0600

    twitter authentication with lot of fixes

--- includes/post-on-twitter.php
+++ includes/post-on-twitter.php
@@ -10,7 +10,6 @@ require_once('../config.php');
 if(isset($_GET['post_on_tw'])) {
     Codebird::setConsumerKey("Kj72AcDtVn5ZChMeEdLZadnbe", "sniKxYn5Lbmiic3oYBsCPOpBBWYV649P850Tv3gsQdKBg9CAxF");
     $cb = Codebird::getInstance();
-
     if(!isset($_SESSION['oauth_token'])) {
         // get the request token
         $reply = $cb->oauth_requestToken(array(
@@ -28,7 +27,7 @@ if(isset($_GET['post_on_tw'])) {
         header('Location: ' . $auth_url);
         die();
 
-    } elseif(isset($_GET['oauth_verifier']) && isset($_SESSION['oauth_verify'])) {
+    } else if(isset($_GET['oauth_verifier']) && isset($_SESSION['oauth_verify'])) {
         // verify the token
         $cb->setToken($_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
         unset($_SESSION['oauth_verify']);
@@ -41,23 +40,24 @@ if(isset($_GET['post_on_tw'])) {
         // store the token (which is different from the request token!)
         $_SESSION['oauth_token'] = $reply->oauth_token;
         $_SESSION['oauth_token_secret'] = $reply->oauth_token_secret;
-    }
+        // assign access token on each page load
+        $cb->setToken($_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
 
-// assign access token on each page load
-    $cb->setToken($_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
-
-    if(isset($_GET['picture'])) {
-        $picture = $_GET['picture'];
+        if(isset($_GET['picture'])) {
+            $picture = $_GET['picture'];
+        }
+        if(isset($_GET['message'])) {
+            $message = $_GET['message'];
+        }
+        $reply = $cb->media_upload(array(
+            'media' => $picture
+        ));
+        $reply = $cb->statuses_update(array(
+            'status' => urldecode(substr(urlencode(resolveHasTags($message)), 0, 120)),
+            'media_ids' => $reply->media_id_string
+        ));
+        unset($_SESSION['oauth_token']);
+        header("Location: $base_url$context_uri?close_window=true");
+        die();
     }
-    if(isset($_GET['message'])) {
-        $message = $_GET['message'];
-    }
-    $reply = $cb->media_upload(array(
-        'media' => $picture
-    ));
-    $reply = $cb->statuses_update(array(
-        'status' => urldecode(substr(urlencode(resolveHasTags($message)), 0, 120)),
-        'media_ids' => $reply->media_id_string
-    ));
 }
-header("Location: $base_url$context_uri?close_window=true");
